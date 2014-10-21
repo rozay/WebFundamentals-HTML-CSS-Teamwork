6c32cd5b315fd2959a1454e76d7fe8dadf0390a3
Nikola Nikolov <nikola_00219@abv.bg>
2014-10-21 03:41:15 +0300

    updated PHP template

--- php template/footer.php
+++ php template/footer.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-
 <div id="footer-wrapper">
-	<div id="footer-col-1">
-		<ul>
-			<li>Col-1 Линк 1</li>
-			<li>Col-1 Линк 2</li>
-			<li>Col-1 Линк 3</li>
-		</ul>
-	</div>
-	<div id="footer-col-2">
-		<ul>
-			<li>Col-2 Линк 1</li>
-			<li>Col-2 Линк 2</li>
-			<li>Col-2 Линк 3</li>
-		</ul>
-	</div>
-    <div id="copyright">
-        Copyright © 2014 Deep Sky Blue. Всички права запазени.
+    <div id="footer-col-1">
+        <ul>
+            <li>Col-1 Линк 1</li>
+            <li>Col-1 Линк 2</li>
+            <li>Col-1 Линк 3</li>
+        </ul>
     </div>
+    <div id="footer-col-2">
+        <ul>
+            <li>Col-2 Линк 1</li>
+            <li>Col-2 Линк 2</li>
+            <li>Col-2 Линк 3</li>
+        </ul>
+    </div>
+    <div id="copyright"> Copyright © 2014 Deep Sky Blue. Всички права запазени.</div>
 </div>
--- php template/jokes.php
+++ php template/jokes.php
@@ -3,19 +3,19 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Начало</title>
-    <link type="text/css" href="style.css" rel="stylesheet" />
+    <link type="text/css" href="style.css" rel="stylesheet"/>
 </head>
 <body>
-    <header>
-		<?php include 'header.php'; ?>
-    </header>
-    <main>
-        <div id="main-wrapper">
-            <h1>Вицове</h1>
-        </div>
-    </main>
-    <footer>
-		<?php include 'footer.php'; ?>
-    </footer>
+<header>
+    <?php include 'header.php'; ?>
+</header>
+<main>
+    <div id="main-wrapper">
+        <h1>Вицове</h1>
+    </div>
+</main>
+<footer>
+    <?php include 'footer.php'; ?>
+</footer>
 </body>
 </html>
