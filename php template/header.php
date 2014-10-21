96ebd0fdb4ff0e4ba12699172d4e49b6c36bab9b
Nikola Nikolov <nikola_00219@abv.bg>
2014-10-21 03:27:46 +0300

    added PHP template

---
+++ php template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<div id="header-wrapper"> 
+	<div id="header-logo">
+        <img src="resources/header.jpg" />
+	</div>
+	<div id="navigation">
+		<nav>
+			<ul>
+                <a href="index.php"><li>Начало</li></a>
+                <a href="guide.php"><li>Наръчник</li></a>
+                <a href="jokes.php"><li>Вицове</li></a>
+                <a href="gallery.php"><li>Галерия</li></a>
+                <a href="about.php"><li>За нас</li></a>
+			</ul>
+		</nav>
+		<a href="#"></a>
+	</div>
+</div>
---
+++ php template/index.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Начало</title>
+    <link type="text/css" href="style.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+		<?php include 'header.php'; ?>
+    </header>
+    <main>
+        <div id="main-wrapper">
+            <h1>Начало</h1>
+        </div>
+    </main>
+    <footer>
+		<?php include 'footer.php'; ?>
+    </footer>
+</body>
+</html>
