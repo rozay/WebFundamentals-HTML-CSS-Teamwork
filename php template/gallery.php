24bd3357444d1cdaab2c5a9f620936297ae87e04
toshko93 <toshco_93@abv.bg>
2014-10-22 15:08:12 +0300

    Moved images folder to resources/images

--- php template/gallery.php
+++ php template/gallery.php
@@ -12,6 +12,114 @@
 <main>
     <div id="main-wrapper">
         <h1>Галерия</h1>
+        <article>
+            <header><h2>Fixing a bug</h2></header>
+            <div><img src="resources\images\bugFixing.gif"></div>
+        </article>
+        <article>
+            <header><h2>Stages of every task in the exam</h2></header>
+            <div><img src="resources\images\stagesOfEveryTask.jpg"></div>
+        </article>
+        <article>
+            <header><h2>When you left your homework for the last day</h2></header>
+            <div><img src="resources\images\Deathline.gif"></div>
+        </article>
+        <article>
+            <header><h2>Endless loop</h2></header>
+            <div><img src="resources\images\endlessLoop.gif"></div>
+        </article>
+        <article>
+            <header><h2>When something I expected to fail works</h2></header>
+            <div><img src="resources\images\whenSmthIexpectedToFailWorks.gif"></div>
+        </article>
+        <article>
+            <header><h2>First day in SoftUni be like..</h2></header>
+            <div><img src="resources\images\firstDayInSoftUni.gif"></div>
+        </article>
+        <article>
+            <header><h2>Forgot the "break" in a loop</h2></header>
+            <div><img src="resources\images\forgotTheBreakInMyLoop.gif"></div>
+        </article>
+        <article>
+            <header><h2>Letting someone in my code:</h2></header>
+            <div><img src="resources\images\fuck.gif"></div>
+        </article>
+        <article>
+            <header><h2>Looking at my first projects as a programmer</h2></header>
+            <div><img src="resources\images\LookingAtOldProject.gif"></div>
+        </article>
+        <article>
+            <header><h2>Realizing the boss is not joking about what we should do</h2></header>
+            <div><img src="resources\images\realizingTheBossIsNotJokingAboutWhatToDo.gif"></div>
+        </article>
+        <article>
+            <header><h2>Security problems</h2></header>
+            <div><img src="resources\images\security.gif"></div>
+        </article>
+        <article>
+            <header><h2>Senior developer..</h2></header>
+            <div><img src="resources\images\seniorDeveloper.gif"></div>
+        </article>
+        <article>
+            <header><h2>Finding out what is snippet</h2></header>
+            <div><img src="resources\images\snippetsForFirstTime.gif"></div>
+        </article>
+        <article>
+            <header><h2>Reading the tasks at the exam</h2></header>
+            <div><img src="resources\images\readingExamProblems.gif"></div>
+        </article>
+        <article>
+            <header><h2>Nothing can scatter me while I study for the exam</h2></header>
+            <div><img src="resources\images\studyForExam.gif"></div>
+        </article>
+        <article>
+            <header><h2>Uncaught exeption</h2></header>
+            <div><img src="resources\images\uncaughtExeption.gif"></div>
+        </article>
+        <article>
+            <header><h2>Knowing that I have bug in my code</h2></header>
+            <div><img src="resources\images\unexpectetErrorFlag.gif"></div>
+        </article>
+        <article>
+            <header><h2>First submit in judge at the exam always like..</h2></header>
+            <div><img src="resources\images\firstTryInJudge.gif"></div>
+        </article>
+        <article>
+            <header><h2>After reading what am I supposed to do</h2></header>
+            <div><img src="resources\images\what.gif"></div>
+        </article>
+        <article>
+            <header><h2>When bug becomes feature</h2></header>
+            <div><img src="resources\images\whenBugBecomesFeature.gif"></div>
+        </article>
+        <article>
+            <header><h2>When colleague makes update on my code</h2></header>
+            <div><img src="resources\images\whenCollegeMakesUpdateOnMyCode.gif"></div>
+        </article>
+        <article>
+            <header><h2>"Do you want Internet Explorer to be your default browser?"</h2></header>
+            <div><img src="resources\images\WhenIEasks.gif"></div>
+        </article>
+        <article>
+            <header><h2>When I missclick the taskbar and Photoshop starts to load</h2></header>
+            <div><img src="resources\images\whenImisclickTheTaskbarAndPhotoshopStartsToLoad.gif"></div>
+        </article>
+        <article>
+            <header><h2>When I compile my code for the first time</h2></header>
+            <div><img src="resources\images\whenMyCodeCompilesOnFirstTime.gif"></div>
+        </article>
+        <article>
+            <header><h2>When SoftUni gives us a present</h2></header>
+            <div><img src="resources\images\WhenSoftUniGaveUsPresents.gif"></div>
+        </article>
+        <article>
+            <header><h2>Asking who will defend the project, everyone be like..</h2></header>
+            <div><img src="resources\images\whoWillDefTheProject.gif"></div>
+        </article>
+        <article>
+            <header><h2>Software product</h2></header>
+            <div><img src="resources\images\softwareProduct.jpg"></div>
+        </article>
     </div>
 </main>
 <footer>
