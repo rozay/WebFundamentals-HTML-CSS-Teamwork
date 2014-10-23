cee7d1a186c31b4b64d2652b6134c2cab3559330
Nikola Nikolov <nikola_00219@abv.bg>
2014-10-23 18:46:47 +0300

    fixed html5 validation

--- php template/templates/header.php
+++ php template/templates/header.php
@@ -1,17 +1,46 @@
 <div id="header-wrapper">
     <div id="header-logo">
-        <img src="resources/header.jpg"/>
+        <img src="resources/header.jpg" alt="background image"/>
     </div>
     <div id="navigation">
         <nav>
             <ul>
-                <a href="index.php"><li>Начало</li></a>
-                <a href="guide.php"><li>Наръчник</li></a>
-                <a href="jokes.php"><li>Вицове</li></a>
-                <a href="gallery.php"><li>Галерия</li></a>
-                <a href="about.php"><li>За нас</li></a>
+                <li>
+                    <a href="index.php">
+                        <div>Начало</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="guide.php">
+                        <div>Наръчник</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="jokes.php">
+                        <div>Вицове</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="gallery.php">
+                        <div>Галерия</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="about.php">
+                        <div>За нас</div>
+                    </a>
+                </li>
             </ul>
         </nav>
-        <a href="#"></a>
+<!--        <a href="#">-->
+<!--            <svg x="0px" y="0px" width="35px" height="25px" viewBox="0 0 35 25" enable-background="new 0 0 35 25" xml:space="preserve">-->
+<!--                <defs></defs>-->
+<!--                <line id="topLine" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="2.5" y1="14.5" x2="17.5" y2="2.5"></line>-->
+<!--                <line id="bottomLine" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="17.5" y1="2.5" x2="32.5" y2="14.5"></line>-->
+<!---->
+<!--                <line id="topLine" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="2.5" y1="22.5" x2="17.5" y2="10.5"></line>-->
+<!--                <line id="bottomLine" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="17.5" y1="10.5" x2="32.5" y2="22.5"></line>-->
+<!--            </svg>-->
+<!--        </a>-->
     </div>
 </div>
