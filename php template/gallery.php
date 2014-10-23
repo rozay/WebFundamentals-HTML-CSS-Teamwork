494c439b3dadbf769b8c1bcf5d82a32fbe05ed13
Nikola Nikolov <nikola_00219@abv.bg>
2014-10-23 23:32:52 +0300

    added viewport meta for mobile devices

--- php template/gallery.php
+++ php template/gallery.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Галерия</title>
     <link type="text/css" href="styles/style.css" rel="stylesheet"/>
     <script type="text/javascript" src="js/jquery.js"></script>
--- php template/guide.php
+++ php template/guide.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Наръчник</title>
     <link type="text/css" href="styles/style.css" rel="stylesheet"/>
     <link type="text/css" href="styles/style-guide.css" rel="stylesheet"/>
